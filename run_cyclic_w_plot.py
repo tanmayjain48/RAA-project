057350b7ee5c8fb3e93a4ed7aa479137a19c2cba
tanmayjain48 <74394352+tanmayjain48@users.noreply.github.com>
2023-09-08 00:19:58 -0400

    Add files via upload

---
+++ run_cyclic_w_plot.py
@@ -0,0 +1,66 @@
+from potentiostat import Potentiostat
+import matplotlib.pyplot as plt
+
+port = '/dev/ttyACM0'       # Serial port for potentiostat device
+datafile = '20230906_CV_TJ trial-300mM-3.txt'       # Output file for time, curr, volt data
+
+
+test_name = 'cyclic'        # The name of the test to run
+curr_range = '1000uA'        # The name of the current range [-100uA, +100uA]
+sample_rate = 300.0         # The number of samples/second to collect
+
+volt_min = -0.5             # The minimum voltage in the waveform (V)
+volt_max =  0.8             # The maximum voltage in the waveform (V)
+#volt_per_sec = 0.050        # The rate at which to transition from volt_min to volt_max (V/s)
+volt_per_sec = 0.500         # The rate at which to transition from volt_min to volt_max (V/s)
+num_cycles = 5              # The number of cycle in the waveform
+
+# Convert parameters to amplitude, offset, period, phase shift for triangle waveform
+amplitude = (volt_max - volt_min)/2.0            # Waveform peak amplitude (V) 
+offset = (volt_max + volt_min)/2.0               # Waveform offset (V) 
+period_ms = int(1000*4*amplitude/volt_per_sec)   # Waveform period in (ms)
+shift = 0.0                                      # Waveform phase shift - expressed as [0,1] number
+                                                 # 0 = no phase shift, 0.5 = 180 deg phase shift, etc.
+
+# Create dictionary of waveform parameters for cyclic voltammetry test
+test_param = {
+        'quietValue' : 0.0,
+        'quietTime'  : 0,
+        'amplitude'  : amplitude,
+        'offset'     : offset,
+        'period'     : period_ms,
+        'numCycles'  : num_cycles,
+        'shift'      : shift,
+        }
+
+# Create potentiostat object and set current range, sample rate and test parameters
+dev = Potentiostat(port)     
+dev.set_curr_range(curr_range)   
+dev.set_sample_rate(sample_rate)
+dev.set_param(test_name,test_param)
+
+# Run cyclic voltammetry test
+#t,volt,curr = dev.run_test(test_name,display='pbar',filename=datafile)
+t,volt,curr = dev.run_test(test_name,display='data',filename=datafile)
+
+# plot results using matplotlib
+plt.figure(1)
+plt.subplot(211)
+plt.plot(t,volt)
+plt.ylabel('potential (V)')
+plt.grid('on')
+
+plt.subplot(212)
+plt.plot(t,curr)
+plt.ylabel('current (uA)')
+plt.xlabel('time (sec)')
+plt.grid('on')
+
+plt.figure(2)
+plt.plot(volt,curr)
+plt.xlabel('potential (V)')
+plt.ylabel('current (uA)')
+plt.grid('on')
+
+plt.show()
+
